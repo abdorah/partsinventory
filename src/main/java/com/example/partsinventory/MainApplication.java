6f31ff1d4f7623bf81cecbbf7fa65a114e5fa3c8
kotbi mohamed <simojetta2@gmail.com>
2023-12-14 12:17:42 -0800

    bissmillah

---
+++ src/main/java/com/example/partsinventory/MainApplication.java
@@ -0,0 +1,26 @@
+package com.example.partsinventory;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("intro-pane.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        Image icon=new Image("C:\\code\\desktop application\\partsinventory\\src\\main\\resources\\images\\icon.jpg");
+        stage.getIcons().add(icon);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
